15c3c1823f2fa5bb891870ad8e21a0ad10c2ba19
Erick Valente <erickhwk@gmail.com>
2022-05-28 09:14:12 -0300

    Dark UI style implemented

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
   has_one_attached :image_url
   validates :name, presence: true, uniqueness: true
+  validates :image_url, presence: true
 end
